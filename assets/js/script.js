8f4d7bd386840e10af44ea5ebf3b2dac1bede4b7
Kristian Lang <kristian.lang123@gmail.com>
2024-06-03 16:01:43 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,24 @@ $(document).ready(function () {
     $(this).toggleClass("fa-times");
     $("header").toggleClass("toggle");
   });
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
 });
