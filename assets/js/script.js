fe8c555bd2c2c342577f48c9d2621e0fab598bdf
Kristian Lang <kristian.lang123@gmail.com>
2024-07-23 17:34:11 +0200

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,45 @@ $(document).ready(function () {
     $(this).toggleClass("fa-times");
     $("header").toggleClass("toggle");
   });
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:3,
-            nav:true,
-            loop:false
-        }
-    }
-})
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
+  var textWrapper = document.querySelector(".ml10 .letters");
+  textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+
+  anime
+    .timeline({ loop: true })
+    .add({
+      targets: ".ml10 .letter",
+      rotateY: [-90, 0],
+      duration: 1300,
+      delay: (el, i) => 45 * i,
+    })
+    .add({
+      targets: ".ml10",
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000,
+    });
 });
